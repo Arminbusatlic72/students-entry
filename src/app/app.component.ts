0e3d55fc58123c78ab3cd1925dc0b2da48371072
Armin <busatar@cronos.be>
2025-02-10 19:23:55 +0100

    adding selectors to display data in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { StudentsTableComponent } from './students-table/students-table/students-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [StudentsTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'students-entry';
---
+++ src/app/students-table/students-table/students-table.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+// import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { Store } from '@ngrx/store';
+import { Observable, Subject } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
+import { StudentsRecords } from '../../state/students-record';
+import { AppState, selectAll } from '../../state/students-selectors';
+
+@Component({
+  selector: 'app-students-table',
+  standalone: true,
+  imports: [MatTableModule, MatInputModule, MatFormFieldModule],
+  templateUrl: './students-table.component.html',
+  styleUrl: './students-table.component.scss',
+})
+export class StudentsTableComponent implements OnInit {
+  dataSource: any = [];
+  dataSource$: Observable<StudentsRecords[]>;
+  displayColumns: string[] = [
+    'name',
+    'city',
+    'country',
+    'subject',
+    'passportDeclaration',
+    'fitnessDeclaration',
+    'courseName',
+    'date',
+    'state',
+    'street',
+    'email',
+    'phone',
+    'postalCode',
+  ];
+  constructor(private store: Store<AppState>) {
+    this.dataSource$ = this.store.select(selectAll);
+  }
+  ngOnInit(): void {
+    this.dataSource$.subscribe((res: any) => {
+      this.dataSource = res;
+    });
+  }
+}
